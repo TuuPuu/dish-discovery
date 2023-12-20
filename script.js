618af22563697961e414968fe50bebe9fbcac0ba
celia103 <celiayych@gmail.com>
2023-12-20 19:56:12 +0000

    draft fetch

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// HIYA, JAVASCRIPT IN HERE PLEASE :p
+
+function getRandomMeal() {
+  fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // Handle the API response data here
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+}
